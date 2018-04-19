a8db2eca3787c3d95cdeb2a7982d4345dfcd65c9
Mateo Bošnjak <31937111+bosnjakmateo@users.noreply.github.com>
2018-04-19 13:52:32 +0200

    Kontrole za micanje igrača
    Dodana testna scena te skripta za kontrole micanja igrača

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    float walkingSpeed;
+    [SerializeField]
+    float rotationSpeed;
+
+    float runningSpeed;
+
+	// Use this for initialization
+	void Start () {
+        runningSpeed = walkingSpeed * 1.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        float z;
+
+        if (Input.GetKey("left shift")) {
+            z = Input.GetAxis("Vertical") * Time.deltaTime * runningSpeed;
+        } else {
+            z = Input.GetAxis("Vertical") * Time.deltaTime * walkingSpeed;
+        }
+            
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
