d8cc623a1ff7e238ae2f361da8c3e61201277c65
Mateo Bošnjak <31937111+bosnjakmateo@users.noreply.github.com>
2018-04-26 16:53:52 +0200

    Uređena player skripta; Uključen "Root motion"; Dodan pathing putem nav mesha
    Dodane razne "Handle" funkcije unutar player skripte; Omogućeno toggle-anje photo mode-a; Dodan enemy pathing pomoću nav mash-a

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform[] enemyPath;
+    public NavMeshAgent agent;
+    int i = 0;
+
+	void Update () {
+        agent.SetDestination(enemyPath[i % 5].position);
+
+        if(transform.position.x == enemyPath[i%5].position.x && transform.position.z == enemyPath[i % 5].position.z) {
+            i++;
+        }
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,73 +5,84 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField]
-    float walkingSpeed;
-    [SerializeField]
-    float rotationSpeed;
-
-    float runningSpeed;
+    public float rotationSpeed;
 
     Animator anim;
 
+    public GameObject photoModeUI;
     public Image batteryLife;
     public Image fadeImage;
 
-    [SerializeField]
-    private float startingTime;
+    bool isPhotoModeActive = false;
+
+    public float startingTime;
     private float timeLeft;
 
-	// Use this for initialization
-	void Start () {
-        runningSpeed = walkingSpeed * 1.5f;
 
+	void Start () {
         anim = GetComponent<Animator>();
 
+        photoModeUI.SetActive(false);
         timeLeft = startingTime;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        if(Input.GetAxis("Vertical") != 0) {
+        HandleAnimatorStates();
+        HandlePhotoMode();
+    }
+
+    private void FixedUpdate () {
+        HandleRotation();
+    }
+
+    void HandleRotation () {
+        float rotationY = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(0, rotationY, 0);
+    }
+
+    void HandleAnimatorStates () {
+        //Walking animaton
+        if (Input.GetAxis("Vertical") != 0) {
             anim.SetBool("isWalking", true);
         } else {
             anim.SetBool("isWalking", false);
         }
 
-        //Crouch
-        if(Input.GetKey("c")){
-            anim.SetBool("onGround", true);
-        }else{
-            anim.SetBool("onGround", false);
-        }
-
-
-        //Fade trasition
-        if (Input.GetKey("f")) {
-            StartCoroutine(FadeIn());
-        }
-
-        //Battery Life
-        timeLeft -= Time.deltaTime;
-        batteryLife.fillAmount = timeLeft / startingTime;
-	}
-
-    private void FixedUpdate () {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-        float z;
-
+        //Running animation
         if (Input.GetKey("left shift")) {
-            z = Input.GetAxis("Vertical") * Time.deltaTime * runningSpeed;
+            anim.SetBool("isRunning", true);
         } else {
-            z = Input.GetAxis("Vertical") * Time.deltaTime * walkingSpeed;
+            anim.SetBool("isRunning", false);
         }
-            
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+
+        //Crouch Animation
+        if (Input.GetKey("c") || Input.GetKey("left ctrl")) {
+            anim.SetBool("isCrouching", true);
+        } else {
+            anim.SetBool("isCrouching", false);
+        }
     }
 
+    void HandlePhotoMode () {
+        if (Input.GetKeyDown("f")) {
+            StartCoroutine(FadeIn());
+            isPhotoModeActive = !isPhotoModeActive;
+        }
+
+        /*Photo mode UI se samo prikazuje kad je aplha slike veca od 90%, tj. kada je korisniku ekran dovoljno
+        zamracen da ne primjeti pojavljivanje UI-a*/
+        if(fadeImage.color.a >= 0.9) {
+            photoModeUI.SetActive(isPhotoModeActive);
+        }
+        
+        timeLeft -= Time.deltaTime;
+        batteryLife.fillAmount = timeLeft / startingTime;
+    }
+
+
+    //Photo mode transition
     IEnumerator FadeIn () {
-        for(float i = 0; i <= 1; i += Time.deltaTime) {
+        for (float i = 0; i <= 1; i += Time.deltaTime) {
             fadeImage.color = new Color(0, 0, 0, i);
  
             yield return null;
