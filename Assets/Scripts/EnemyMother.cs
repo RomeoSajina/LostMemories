f32dba9da0c3b460205609f4529b87e77be408bf
Mateo Bošnjak <31937111+bosnjakmateo@users.noreply.github.com>
2018-05-15 16:42:18 +0200

    Dodano ponašanje majke u levelu Fantays Room

---
+++ Assets/Scripts/EnemyMother.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMother : MonoBehaviour {
+
+    public NavMeshAgent agent;
+
+    public Transform firePlaceLocation;
+    public Transform raycastStart;
+    public Transform raycastTarget;
+
+    private void Update () {
+        HandleRaycasting();
+    }
+
+    void HandleRaycasting () {
+        Debug.DrawLine(raycastStart.position, raycastTarget.position - transform.position, Color.blue);
+        //Debug.Log(raycastTarget.position);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycastStart.position, raycastTarget.position - transform.position, out hit, 5)) {
+            //Debug.Log(hit.transform.tag);
+            if (hit.transform.tag == "Player") {
+                Debug.Log("Player");
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void HandleAlert () {
+        agent.SetDestination(firePlaceLocation.position);
+        /*Vector3 targetPostition = new Vector3(firePlaceLocation.position.x,
+                                       this.transform.position.y,
+                                       firePlaceLocation.position.z);
+        this.transform.LookAt(targetPostition);*/
+    }
+
+    public IEnumerator StartAlert (int seconds) {
+        yield return new WaitForSeconds(seconds);
+        HandleAlert();
+    }
+}
---
+++ Assets/Scripts/Fireplace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour {
+
+    public GameObject fire;
+    public EnemyMother enemyMother;
+
+    private void OnTriggerStay (Collider other) {
+        if (other.CompareTag("Player") && Input.GetKeyDown("e")) {
+            Debug.Log("Vatra ugasena/upaljena");
+            fire.SetActive(false);
+
+            StartCoroutine(enemyMother.StartAlert(2));
+        }
+    }
+}
