3b6afd54c006081b5d414e2f57e6496a1d7303af
Mateo Bošnjak <31937111+bosnjakmateo@users.noreply.github.com>
2018-05-17 12:16:35 +0200

    Popravljen canvas i dodan objekt za prikazivanje hinta

---
+++ Assets/Scripts/HintTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintTrigger : MonoBehaviour {
+
+    public Text hintText;
+    public string hint;
+
+    private bool hasTriggered = false;
+
+    private void OnTriggerExit (Collider other) {
+        if (other.CompareTag("Player") && !hasTriggered) {
+            hasTriggered = true;
+            hintText.text = hint;
+
+            StartCoroutine(DisableHint(3));
+        }
+    }
+
+    public IEnumerator DisableHint (int seconds) {
+        yield return new WaitForSeconds(seconds);
+        hintText.text = "";
+    }
+
+}
