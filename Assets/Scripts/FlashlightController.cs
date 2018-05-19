d29e2d2cfe78fcb4788e2df8b2ab3e975fb320b7
Romeo <romeo.sajina@gmail.com>
2018-05-19 15:34:12 +0200

    flashlight

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour {
+
+    private Light light;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.L))
+            light.enabled = !light.enabled;
+	}
+}
